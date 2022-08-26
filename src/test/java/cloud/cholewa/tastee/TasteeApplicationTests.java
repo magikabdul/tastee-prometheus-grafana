87be636bc092d3c0a3f37b8e4096d3c4a9d27057
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-08-26 23:04:08 +0200

    initial state

---
+++ src/test/java/cloud/cholewa/tastee/TasteeApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.tastee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
